ef551bcddfeb13e039dafd11937b8fcdc4cf3565
GitGuru420 <raisulislam1892@gmail.com>
2024-11-18 02:06:13 +0530

    String in C++

---
+++ Week-1 (Introduction to C++)/Practice-M01/H-String.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a name: ";
+    cin >> str;
+    cout << "Hello, " << str << endl;
+    
+    return 0;
+}
