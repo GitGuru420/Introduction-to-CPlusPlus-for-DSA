a5c1eec28bcb5dc9cdf7907fe2a69181e44c829d
GitGuru420 <raisulislam1892@gmail.com>
2024-11-18 01:28:19 +0530

    Basic structure of C++ program and input output

---
+++ Week-1 (Introduction to C++)/Practice-M01/A-Basic-Structure-Input-Output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // output
+    cout << "Hello, C++ Program" << endl;
+
+    // input
+    int age;
+    cin >> age;
+    cout << "The programmer age is " << age << endl;
+
+    return 0;
+}
