0da1b5d238c8824d380a623961be5b13cfc89f9f
GitGuru420 <raisulislam1892@gmail.com>
2024-11-17 18:10:33 +0530

    String in C++

---
+++ Week-1 (Introduction to C++)/Module-01 (Basic C++)/L-String.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int x;
+    // cin >> x;
+    // cin.ignore();
+    // char str[100];
+    // // cin >> str;
+    // cin.getline(str, 100);
+    // cout << x << endl << "Hello, " << str << endl;
+
+    string str;
+    cin >> str;
+    cout << str;
+
+    return 0;
+} 
