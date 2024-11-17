ce8e0de8660561f6c684c4bbfb4a8a5276e00073
GitGuru420 <raisulislam1892@gmail.com>
2024-11-17 16:58:42 +0530

    How to print in C++

---
+++ Week-1 (Introduction to C++)/Module-01 (Basic C++)/B-Print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+    int x = 100;
+    char c = 'A';
+    double d = 34.56;
+
+    std::cout << x << std::endl << c << std::endl << d << std::endl;
+    // My favorite number is 169105
+    int y = 169105;
+    std::cout << "My favorite number is " << y << std::endl;
+    
+    return 0;
+}
