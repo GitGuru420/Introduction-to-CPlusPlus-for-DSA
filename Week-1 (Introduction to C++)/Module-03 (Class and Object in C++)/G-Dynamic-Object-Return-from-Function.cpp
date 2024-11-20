46c97e25762648603af69fb8bb86606c23e67250
GitGuru420 <raisulislam1892@gmail.com>
2024-11-20 21:46:52 +0530

    Dynamic Object Return From Function

---
+++ Week-1 (Introduction to C++)/Module-03 (Class and Object in C++)/G-Dynamic-Object-Return-from-Function.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student {
+    public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll, int cls, double gpa) {
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
+
+Student* fun() {
+    Student* karim = new Student(5, 5, 4.5);
+    return karim;
+}
+int main() {
+    Student* p = fun();
+    cout << p->roll << " " << p->cls << " " << p->gpa << endl;
+
+    return 0;
+}
