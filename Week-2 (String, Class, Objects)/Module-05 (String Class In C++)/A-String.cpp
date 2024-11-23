e863c1ba1be3ca6e7bc7e8b472b066cf9fe8293a
GitGuru420 <raisulislam1892@gmail.com>
2024-11-24 01:10:24 +0530

    String Basic in C++

---
+++ Week-2 (String, Class, Objects)/Module-05 (String Class In C++)/A-String.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    /*  C Code
+    char s[10] = "Hello";
+    // s = "Gello"; // error
+    // error sol^n
+    strcpy(s, "Gello");
+    cout << s << endl;
+    */
+
+    /* C++ Code
+    string s = "Hello";
+    s = "Gello";
+    cout << s << endl;
+    */
+
+    /* C Code
+    char s1[100] = "Hello";
+    char s2[100] = "Hello";
+    if(strcmp(s1, s2) == 0) {
+        cout << "Same" << endl;
+    }
+    else {
+        cout << "Not Same" << endl;
+    }
+    */
+    
+
+    /* C++ Code
+    string s1 = "Hello";
+    string s2 = "Hello";
+    if(s1 == s2) {
+        cout << "Same" << endl;
+    }
+    else {
+        cout << "Not Same" << endl;
+    }
+    */
+   
+    string s = "Hello";
+    s = "My name is Raisul Islam";
+    cout << s << endl;
+    
+    return 0;
+}
