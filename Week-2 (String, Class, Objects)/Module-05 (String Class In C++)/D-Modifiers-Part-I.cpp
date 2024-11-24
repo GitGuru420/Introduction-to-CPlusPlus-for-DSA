25ab8ff6d0fa0c4ba9cff6631a3f7625286c54f4
GitGuru420 <raisulislam1892@gmail.com>
2024-11-24 16:15:08 +0530

    String Modifiers

---
+++ Week-2 (String, Class, Objects)/Module-05 (String Class In C++)/D-Modifiers-Part-I.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // string s1, s2;
+    // cin >> s1 >> s2;  
+    
+    // string add
+    // s1 += s2;
+    // s1.append(s2);
+    // cout << s1 << endl;
+
+    // insert a character in a string last
+    // s1.push_back('A');
+    // s1 += 'A';
+    // cout << s1 << endl;
+
+    string s = "Hello";
+    // s[1] = 'h';
+    // s[5] = 'W';
+
+    s.pop_back();
+    cout << s << endl;
+
+
+    return 0;
+}
