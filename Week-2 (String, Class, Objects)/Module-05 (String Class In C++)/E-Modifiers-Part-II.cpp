b2cd468ea830a58cf832542966d9cdd57b34c112
GitGuru420 <raisulislam1892@gmail.com>
2024-11-24 16:42:18 +0530

    String Modifiers

---
+++ Week-2 (String, Class, Objects)/Module-05 (String Class In C++)/E-Modifiers-Part-II.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // string s = "Hello";
+    // s = "Gello";
+    // string s2 = "Hi";
+    // s = s2;
+    // s.assign(s2);
+    // s.assign("Hi Hello");
+
+    string s = "Hello World";
+    // s.erase(5);
+    // s.erase(4, 4);
+    // s.replace(6, 5, "Bangladesh");
+    // s.replace(6, 0, "Bangladesh ");
+    s.insert(5, "Raisul");
+    cout << s << endl;
+
+
+    return 0;
+}
