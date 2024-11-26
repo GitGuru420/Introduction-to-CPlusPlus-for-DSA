34fa04c1c5484bdba7d2ef25198a9b6627bf12cd
GitGuru420 <raisulislam1892@gmail.com>
2024-11-26 11:01:10 +0530

    C++ Setprecision

---
+++ Week-01 (Introduction to C++)/Module-01 (Basic C++)/Lecture/E_Setprecision.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+    // double d = 34.2354;
+    double d = 34.23545;
+    // cout << d << endl;
+    // cout << fixed << setprecision(2) << d << endl;
+    // cout << fixed << setprecision(3) << d << endl;
+    cout << fixed << setprecision(10) << d << endl;
+
+    return 0;
+}
