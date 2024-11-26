73749b5f1662f994dabd50f712a11c1cf871080a
GitGuru420 <raisulislam1892@gmail.com>
2024-11-27 01:57:55 +0530

    Basic String

---
+++ Week-01 (Introduction to C++)/Module-01 (Basic C++)/Practice/F_String.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Your age: " << age << endl;
+    cin.ignore();
+    char s[100];
+    cout << "Enter your name: ";
+    // cin >> s;
+    cin.getline(s, 100);
+    cout << "Hello, " << s << endl;
+
+    string s1;
+    cout << "Enter your name: ";
+    cin >> s1;
+    cout << "Hello, Mr. " << s1 << endl;
+
+    return 0;
+}
