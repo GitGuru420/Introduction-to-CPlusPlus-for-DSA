46102691a65fca6d73e49ca6a7b92b9f98fd15dc
GitGuru420 <raisulislam1892@gmail.com>
2024-11-26 10:44:03 +0530

    C++ Printing

---
+++ Week-01 (Introduction to C++)/Module-01 (Basic C++)/Lecture/B_Print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+    std::cout << "Hello, C++ Programming Language" << std::endl;
+    // endl --> creates a new line
+    int x = 10;
+    char c = 'A';
+    double d = 34.56;
+    std::cout << x << " " << c << " " << d << std::endl;
+    // My favorite number is 169105
+    int number = 169105;
+    std::cout << "My favorite number is " << number << std::endl;
+    return 0;
+}
