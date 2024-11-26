b2fe09c524b63ab5df1c8a96e3fc5cbdffb36acf
GitGuru420 <raisulislam1892@gmail.com>
2024-11-27 01:14:52 +0530

    Basic C++ : Input, Output and Typecasting

---
+++ Week-01 (Introduction to C++)/Module-01 (Basic C++)/Practice/A_Basic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // print in C++
+    cout << "Hello C++ Programming Language" << endl;
+
+    // input & output in C++
+    int number;
+    cout << "Enter an integer number: ";
+    cin >> number;
+    cout << "The entered number is: " << number << endl;
+
+    // typecasting in C++
+    char c;
+    cout << "Enter any character from A to Z: ";
+    cin >> c;
+    cout << "The entered character ASCII value is: " << (int)c << endl;
+
+    return 0;
+}
