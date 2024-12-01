a80af2414922795300240d1c9e556e697054add3
GitGuru420 <raisulislam1892@gmail.com>
2024-12-02 01:02:04 +0530

    String Constructor

---
+++ Week-02 (String, Class, Objects)/Module-06 (More about class and string)/Lecture/A_String_Constructor.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // string s = "Hello";
+    // way-1
+    // string s("Hello");
+    // way-2
+    // string s("Hello World", 4);
+    // cout << s << endl;
+    // way-3
+    // string s = "Hello World";
+    // string t(s, 4);
+    // string t(s, 2);
+    // cout << t << endl;
+
+    // way-4
+    string s(5, 'A');
+    cout << s << endl;
+    
+
+    return 0;
+}
