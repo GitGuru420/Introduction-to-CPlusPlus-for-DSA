edec98ec6e5763e1b016585d37c2b5f81497edb6
GitGuru420 <raisulislam1892@gmail.com>
2024-12-01 12:21:31 +0530

    String Capacity

---
+++ Week-02 (String, Class, Objects)/Module-05 (String Class in C++)/Lecture/B_String_Capacity.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Hi, Hello, Hi";
+    // cout << s.size() << endl;
+    // cout << s.max_size() << endl;
+    // cout << s.capacity() << endl;
+    // s.clear();
+    // cout << s << endl;
+    // if(s.empty() == true) {
+    //     cout << "Empty" << endl;
+    // }
+    // else {
+    //     cout << "Not Empty" << endl;
+    // }
+
+    // s.resize(6);
+    // s.resize(20);
+    // s.resize(20, 'R');
+    cout << s << endl;
+
+    return 0;
+}
