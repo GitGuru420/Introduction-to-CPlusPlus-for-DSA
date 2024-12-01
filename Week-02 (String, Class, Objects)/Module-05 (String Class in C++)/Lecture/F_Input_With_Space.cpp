71c298b4f1a3183d021b71d2f0cd2730822bcab1
GitGuru420 <raisulislam1892@gmail.com>
2024-12-01 13:12:27 +0530

    Input with space

---
+++ Week-02 (String, Class, Objects)/Module-05 (String Class in C++)/Lecture/F_Input_With_Space.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cin.ignore();
+    string s;
+    // cin >> s; 
+    getline(cin, s);
+    // cin >> s;
+    cout << x << endl;
+    cout << s << endl;
+
+    return 0;
+}
